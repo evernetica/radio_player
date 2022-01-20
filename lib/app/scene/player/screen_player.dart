14edf7bbfc168c67cc9bab37fa5b86d513b1fd27
developer <dev@dev>
2022-01-20 18:36:25 +0200

    added player interface layout

--- lib/app/scene/player/screen_player.dart
+++ lib/app/scene/player/screen_player.dart
@@ -8,10 +8,95 @@ class ScreenPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-      child: const Center(
-        child: Text("Player"),
+        color: Colors.green,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _thumbnail(context),
+            _controlButtonsBar(context),
+          ],
+        ));
+  }
+
+  Widget _thumbnail(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        color: Colors.white,
+        child: FractionallySizedBox(
+          widthFactor: 0.8,
+          child: Align(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                color: Colors.black,
+                child: const FittedBox(
+                  child: Icon(
+                    Icons.image,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  Widget _controlButtonsBar(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.7,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _controlsButton(
+              context, Icons.skip_previous, 1, _previousStationAction),
+          _controlsButton(context, Icons.play_arrow, 2, _playPauseAction),
+          _controlsButton(context, Icons.skip_next, 1, _nextStationAction),
+        ],
+      ),
+    );
+  }
+
+  Widget _controlsButton(
+      BuildContext context, IconData icon, int flex, void Function() callback) {
+    return Expanded(
+      flex: flex,
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black, width: 5.0)),
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateColor.resolveWith((states) => Colors.black54),
+                shape:
+                    MaterialStateProperty.all<CircleBorder>(const CircleBorder()),
+              ),
+              onPressed: callback,
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: FittedBox(
+                  child: Icon(
+                    icon,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _playPauseAction() {}
+
+  void _nextStationAction() {}
+
+  void _previousStationAction() {}
 }
