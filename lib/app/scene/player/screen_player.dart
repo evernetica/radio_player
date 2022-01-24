c048abdf3b951966492ead88eb61f2e324514dbb
developer <dev@dev>
2022-01-24 10:39:09 +0200

    fixed image not scaling

--- lib/app/scene/player/screen_player.dart
+++ lib/app/scene/player/screen_player.dart
@@ -30,55 +30,61 @@ class ScreenPlayer extends StatelessWidget {
   }
 
   Widget _thumbnail(BuildContext context, PlayerScreenState state) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: FractionallySizedBox(
-        widthFactor: 1,
-        child: Align(
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      clipBehavior: Clip.hardEdge,
-                      child: Stack(
-                        children: [
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Align(
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                flex: 4,
+                child: Stack(
+                  children: [
+                    _thumbnailImage(
+                      image:
                           Image.asset("assets/images/radio_placeholder.jpeg"),
-                          Image.network(
-                            state.currentStationArtUrl,
-                            errorBuilder: (context, child, snapshot) =>
-                                Container(),
-                          ),
-                        ],
+                    ),
+                    _thumbnailImage(
+                      image: Image.network(
+                        state.currentStationArtUrl,
+                        errorBuilder: (context, child, snapshot) =>
+                            Container(),
                       ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      state.currentStationName,
+                      style: const TextStyle(
+                          inherit: false,
+                          color: Colors.black,
+                          fontSize: 24.0),
                     ),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        state.currentStationName,
-                        style: const TextStyle(
-                            inherit: false,
-                            color: Colors.black,
-                            fontSize: 24.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _thumbnailImage({required Image image}) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: FittedBox(
+        fit: BoxFit.cover,
+        clipBehavior: Clip.hardEdge,
+        child: image,
       ),
     );
   }
