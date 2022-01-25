267520042fd196c391914238097756cdcd90a943
developer <dev@dev>
2022-01-25 13:48:28 +0200

    fixed music turning off after turning off screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:audio_service/audio_service.dart';
 import 'package:audio_session/audio_session.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:radio_player/app/app_root.dart';
 import 'package:radio_player/data/repositories/station_info_google_sheets_repository.dart';
@@ -12,12 +14,13 @@ StationInfoUseCase? stationInfoUseCase;
 StreamController<int>? controller;
 AudioPlayer? audioPlayer;
 BaseAudioHandler? audioHandler;
+AudioSession? session;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final session = await AudioSession.instance;
-  await session.configure(const AudioSessionConfiguration.music());
+  session = await AudioSession.instance;
+  await session!.configure(const AudioSessionConfiguration.music());
 
   stationInfoUseCase =
       StationInfoUseCase(repository: StationInfoGoogleSheetsRepositoryImpl());
@@ -25,16 +28,36 @@ void main() async {
   audioPlayer = AudioPlayer();
 
   controller = StreamController<int>();
-  audioHandler = await AudioService.init(
-    builder: () => MyAudioHandler(controller),
-    config: const AudioServiceConfig(
-      androidStopForegroundOnPause: false,
-      androidNotificationChannelId: 'com.mycompany.myapp.channel.audio',
-      androidNotificationChannelName: 'Music playback',
-    ),
-  );
+
+  startForegroundService();
 
   runApp(AppRoot());
+}
+
+void startForegroundService() async {
+  await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
+  await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
+  await FlutterForegroundPlugin.startForegroundService(
+    holdWakeLock: false,
+    onStarted: () async {
+      audioHandler = await AudioService.init(
+        builder: () => MyAudioHandler(controller),
+        config: const AudioServiceConfig(
+          androidStopForegroundOnPause: false,
+          androidNotificationChannelId: 'com.mycompany.myapp.channel.audio',
+          androidNotificationChannelName: 'Music playback',
+        ),
+      );
+    },
+    onStopped: () {},
+    title: "Flutter Foreground Service",
+    content: "This is Content",
+    iconName: "ic_launcher",
+  );
+}
+
+void globalForegroundService() async {
+  debugPrint("====================================== current datetime is ${DateTime.now()} \n\taudio state is: ${audioPlayer!.processingState.name} \n\taudio handler state is: ${audioHandler!.playbackState.stream.value} \n\t connection: ${(await Connectivity().checkConnectivity()).name}");
 }
 
 class MyAudioHandler extends BaseAudioHandler {
